5615b52d8472c73bbd5f41d1a8fe018d56121d40
Sofia Brown <ruthsofiab@gmail.com>
2019-01-30 16:37:46 -0600

    Model Rotation + Switch Platform + Pause

--- Assets/Scripts/AnimationEditing.cs
+++ Assets/Scripts/AnimationEditing.cs
@@ -6,23 +6,38 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Animation))]
 public class AnimationEditing : MonoBehaviour
 {
-    private Animation legacyAnimation;
     public Slider sliderController;
     public Transform dancing, random;
+    public GameObject btnPause;
+    public GameObject btnPlay;
+    public Slider sliderRotation;
+    public Text txtTitle;
 
+    private Animation legacyAnimation;
     private bool playOnStart = true;
+    private bool isPaused = false;
+    private float speed = 1;
+    private float deltaRotation;
+    private float prevRotValue;
+    private Transform objToRotate;
+
+    
 
     void Start()
     {
         //First look for the object that we will be controlling
         LookForObject();
-
-      
     }
 
     private void Update()
     {
-        sliderController.value = legacyAnimation[legacyAnimation.clip.name].normalizedTime;
+        //If it's paused, its speed will be set to zero
+        if (isPaused)
+            legacyAnimation[legacyAnimation.clip.name].speed = 0;
+        else
+            legacyAnimation[legacyAnimation.clip.name].speed = speed;
+
+        sliderController.value = legacyAnimation[legacyAnimation.clip.name].normalizedTime - Mathf.Floor(legacyAnimation[legacyAnimation.clip.name].normalizedTime);
     }
 
     public void OnChangeSlider()
@@ -34,7 +49,16 @@ public class AnimationEditing : MonoBehaviour
     {
         if (string.IsNullOrEmpty(SubmenuManager.idAnimation))
             return;
-        if(SubmenuManager.isDancing)
+
+        string[] temp = SubmenuManager.idAnimation.Split(' ');
+        if (temp[0] == "Hip")
+            txtTitle.text = temp[0] + " " + temp[1];
+        else if(temp[0] == "Low" || temp[0] == "Mma")
+            txtTitle.text = temp[1];
+        else
+            txtTitle.text = temp[0];
+
+        if (SubmenuManager.isDancing)
         {
             random.gameObject.SetActive(false);
             Transform child = dancing.Find(SubmenuManager.idAnimation);
@@ -42,7 +66,7 @@ public class AnimationEditing : MonoBehaviour
             legacyAnimation = child.GetComponent<Animation>();
             dancing.gameObject.SetActive(true);
             SetAnimationEditing();
-
+            objToRotate = child;
         }
         else
         {
@@ -52,6 +76,7 @@ public class AnimationEditing : MonoBehaviour
             child.gameObject.SetActive(true);
             SetAnimationEditing();
             random.gameObject.SetActive(true);
+            objToRotate = child;
         }
     }
 
@@ -64,4 +89,31 @@ public class AnimationEditing : MonoBehaviour
 
         sliderController.onValueChanged.AddListener(delegate { OnChangeSlider(); });
     }
+
+    #region PAUSE
+    public void Pausar(bool _isPaused)
+    {
+        isPaused = _isPaused;
+        btnPause.SetActive(!_isPaused);
+        btnPlay.SetActive(_isPaused);
+    }
+
+    public void OnApplicationPause(bool pause)
+    {
+        Pausar(pause);
+    }
+    #endregion
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public void RotateObject()
+    {
+        deltaRotation = sliderRotation.value - prevRotValue;
+        objToRotate.Rotate(Vector3.up * deltaRotation * 360);
+        prevRotValue = sliderRotation.value;
+    }
+
 }
